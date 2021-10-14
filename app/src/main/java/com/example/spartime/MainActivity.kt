253da1cccfe912a95fa92277addb18b150dd16e5
Filip Golovic <filipgolovic@Administrators-MacBook-Pro.local>
2021-10-15 00:29:18 +0200

    init commit

---
+++ app/src/main/java/com/example/spartime/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spartime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_fragment)
+    }
+}
