99068780b6453aab957839478e7f849d1abcc50f
Filip Golovic <filipgolovic@Administrators-MacBook-Pro.local>
2021-10-23 13:28:04 +0200

    Add viewBinding, get values on edittext key changes, minor main_fragment changes

--- app/src/main/java/com/example/spartime/MainActivity.kt
+++ app/src/main/java/com/example/spartime/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.spartime
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import com.example.spartime.databinding.MainFragmentBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var round = 0
+    var rest = 0
+    var time = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_fragment)
+        val binding = MainFragmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListeners(binding)
+    }
+
+    private fun setupListeners(binding: MainFragmentBinding){
+        binding.edtxtRound.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                round = Integer.parseInt(s.toString())
+                println("amazon $s")
+                println("pablo $round")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+        binding.edtxtRest.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                println("namjera $round")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+        binding.edtxtTime.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                println("trenutak $round")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+        })
+
     }
 }
