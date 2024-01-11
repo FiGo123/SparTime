57ab5862f1eae2d4582b3d94d9686dfb4b86a65b
Pc <filipgolovicc@gmail.com>
2024-01-11 22:21:46 +0100

    DB setup

---
+++ app/src/main/java/com/example/spartime/data/DBHandler.kt
@@ -0,0 +1,43 @@
+package com.example.spartime.data
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import com.example.spartime.data.models.Training
+
+val DATABASE_NAME = "spartime"
+val TABLE_NAME = "training"
+val COL_TITLE = "title"
+val COL_DATE = "date"
+val COL_ID = "id"
+
+class DBHandler (var context: Context) :SQLiteOpenHelper(context, DATABASE_NAME, null, 1){
+    override fun onCreate(p0: SQLiteDatabase?) {
+        val createTable = "CREATE TABLE" + TABLE_NAME + " (" +
+                COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_TITLE + " VARCHAR(256)," +
+                COL_DATE + " VARCHAR(256),";
+
+        p0?.execSQL(createTable)
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun insertData(training: Training){
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COL_TITLE, training.title)
+        cv.put(COL_DATE, training.date)
+        val result = db.insert(TABLE_NAME, null,cv)
+        if (result == -1.toLong())
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
+
+    }
+}
--- app/src/main/java/com/example/spartime/data/models/Training.kt
+++ app/src/main/java/com/example/spartime/data/models/Training.kt
@@ -1,11 +1,21 @@
 package com.example.spartime.data.models
 
-data class Training(
-    var id: Int = -1,
-    var title: String,
-    var date: String,
-    var user: Int, // Foreign key to User
-    var duration: Int,
-    var difficultyScale: Int,
-    var description: String
-)
+import java.time.Duration
+import java.util.Date
+
+class Training{
+    var id: Int = -1
+    var title: String = ""
+    var date: String = ""
+    var duration: Int = -1
+    var difficultyScale: Int = -1
+    var description: String = ""
+
+    constructor(title: String, date: String, duration: Int, dificultyScale: Int, description: String){
+        this.title = title
+        this.date = date
+        this.duration = duration
+        this.difficultyScale = dificultyScale
+        this.description = description
+    }
+}
