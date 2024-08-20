8621de0bca0b846453aa9619778cbed7f34931fb
Pc <filipgolovicc@gmail.com>
2024-08-20 21:58:31 +0200

    Add autoupdate first part

--- app/src/main/java/com/example/spartime/MainActivity.kt
+++ app/src/main/java/com/example/spartime/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.spartime
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.spartime.databinding.FragmentFirstBinding
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +17,39 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private val REQUEST_CODE_UPDATE = 123
+
+    fun checkForUpdates() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+
+                // Request an update
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.IMMEDIATE,
+                    this,
+                    REQUEST_CODE_UPDATE
+                )
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_UPDATE) {
+            if (resultCode != RESULT_OK) {
+                // Handle the update failure - The user might have canceled the update
+                Toast.makeText(this, "Update failed! Please try again later.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
 }
+
