85e04dd129261a1cab1015e00f911faa9381aee2
Pc <filipgolovicc@gmail.com>
2024-08-21 21:37:37 +0200

    Add lottie animation

--- app/src/main/java/com/example/spartime/MainActivity.kt
+++ app/src/main/java/com/example/spartime/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
+import com.example.spartime.databinding.ActivityMainBinding
 import com.example.spartime.databinding.FragmentFirstBinding
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory
 import com.google.android.play.core.install.model.AppUpdateType
@@ -11,9 +13,21 @@ import com.google.android.play.core.install.model.UpdateAvailability
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animationView: LottieAnimationView = binding.boxingGloveTimerAnimation
+
+        // Start animation
+        animationView.playAnimation()
+
+        // Optionally set a listener
+        animationView.addAnimatorUpdateListener { valueAnimator ->
+            // Add custom behavior for updates
+        }
 
     }
 
